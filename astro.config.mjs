7113f95e9121ca75ce13fb4d17b5cb24030867bc
bishal-dd <dhakalbishal930@gmail.com>
2024-09-21 15:35:13 +0600

    config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import vercelStatic from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
-
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
@@ -29,11 +28,8 @@ export default defineConfig({
     sitemap({
       i18n: {
         defaultLocale: "en",
-        // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
         locales: {
           en: "en",
-          // The `defaultLocale` value must present in `locales` keys
-          fr: "fr",
         },
       },
     }),
